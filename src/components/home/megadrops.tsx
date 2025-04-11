a4adf429c4f7ae5d60fd7bd76211fd6f15e1bf11
Viktor Plane <viktor@planevin.se>
2025-04-11 14:31:31 +0200

    fix: order of megadrops

--- src/components/home/megadrops.tsx
+++ src/components/home/megadrops.tsx
@@ -3,8 +3,7 @@ export default function MegaDropsSection() {
       <section className="mt-24 md:mt-32">
           <div className="mx-auto max-w-5xl px-6">
               <div className="grid gap-6 md:grid-cols-2 md:gap-12">
-                  <h2 className="text-4xl font-medium">Active your user-base with on-chain rewards</h2>
-                  <div className="space-y-6">
+                  <div className="order-2 md:order-1 space-y-6">
                       <p>
                           MegaDrops is a decentralized rewards system that allows you to reward users for owning and interacting with your tokens.
                       </p>
@@ -12,6 +11,7 @@ export default function MegaDropsSection() {
                           Reward your users for holding your token, or for interacting with different parts of your ecosystem.
                       </p>
                   </div>
+                  <h2 className="order-1 md:order-2 text-4xl font-medium">Active your user-base with on-chain rewards</h2>
               </div>
           </div>
       </section>
